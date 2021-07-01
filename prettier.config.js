6dc018e2944a45bdd2628da66c528bc5ddd570bb
PitzTech <53540002+PitzTech@users.noreply.github.com>
2021-07-01 08:01:57 -0300

    Update Linters

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	tabWidth: 3,
+	useTabs: true,
+	semi: false,
+	arrowParens: "avoid",
+	//printWidth: 90,
+	trailingComma: "all",
+	proseWrap: "preserve",
+	jsxBracketSameLine: false,
+	//bracketSpacing: false,
+}
