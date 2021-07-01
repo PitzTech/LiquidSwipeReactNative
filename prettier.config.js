85e8463a93db28870d4934eb4f4b609cba4adc19
PitzTech <53540002+PitzTech@users.noreply.github.com>
2021-07-01 08:22:37 -0300

    Refactor

--- prettier.config.js
+++ prettier.config.js
@@ -7,5 +7,5 @@ module.exports = {
 	trailingComma: "all",
 	proseWrap: "preserve",
 	jsxBracketSameLine: false,
-	//bracketSpacing: false,
+	bracketSpacing: true,
 }
--- src/Slider.tsx
+++ src/Slider.tsx
@@ -1,45 +1,45 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
+import React from "react"
+import { StyleSheet, View } from "react-native"
 
-import Wave, { HEIGHT, MARGIN_WIDTH, Side, WIDTH } from "./Wave";
-import Button from "./Button";
+import Wave, { HEIGHT, MARGIN_WIDTH, Side, WIDTH } from "./Wave"
+import Button from "./Button"
 
-const PREV = WIDTH;
-const NEXT = 0;
+const PREV = WIDTH
+const NEXT = 0
 
 interface SliderProps {
-  index: number;
-  setIndex: (value: number) => void;
-  children: JSX.Element;
-  prev?: JSX.Element;
-  next?: JSX.Element;
+	index: number
+	setIndex: (value: number) => void
+	children: JSX.Element
+	prev?: JSX.Element
+	next?: JSX.Element
 }
 
-const Slider = ({
-  index,
-  children: current,
-  prev,
-  next,
-  setIndex,
-}: SliderProps) => {
-  const hasPrev = !!prev;
-  const hasNext = !!next;
+function Slider({
+	index,
+	children: current,
+	prev,
+	next,
+	setIndex,
+}: SliderProps): JSX.Element {
+	const hasPrev = !!prev
+	const hasNext = !!next
 
-  return (
-    <View style={StyleSheet.absoluteFill}>
-      {current}
-      {prev && (
-        <View style={[StyleSheet.absoluteFill]}>
-          <Wave side={Side.LEFT}>{prev}</Wave>
-        </View>
-      )}
-      {next && (
-        <View style={StyleSheet.absoluteFill}>
-          <Wave side={Side.RIGHT}>{next}</Wave>
-        </View>
-      )}
-    </View>
-  );
-};
+	return (
+		<View style={StyleSheet.absoluteFill}>
+			{current}
+			{prev && (
+				<View style={[StyleSheet.absoluteFill]}>
+					<Wave side={Side.LEFT}>{prev}</Wave>
+				</View>
+			)}
+			{next && (
+				<View style={StyleSheet.absoluteFill}>
+					<Wave side={Side.RIGHT}>{next}</Wave>
+				</View>
+			)}
+		</View>
+	)
+}
 
-export default Slider;
+export default Slider
